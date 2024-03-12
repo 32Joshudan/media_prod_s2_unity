c67e019478cbc09d9ef0402161b8e5a63e36f4e8
32Joshudan <114925192+32Joshudan@users.noreply.github.com>
2024-03-12 12:07:43 +0000

    added audio test

---
+++ Assets/Scripts/Temp/CannonBehaviour.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBehaviour : MonoBehaviour
+{
+
+
+    public AudioClip cannonFireSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Cannon");
+
+        GetComponent<AudioSource>().PlayOneShot(cannonFireSound);
+    }
+
+
+}
