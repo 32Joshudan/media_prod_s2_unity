b954e54f3ff8ca1e31a2d9dbee5d94a0dcf98a95
32Joshudan <114925192+32Joshudan@users.noreply.github.com>
2024-03-25 21:35:59 +0000

    added to do list

---
+++ Assets/notices_and_to_do.cs
@@ -0,0 +1,8 @@
+/*
+DELETE LATER WHEN PROJECT IS COMPLETED
+
+- Add more NPCs. Use the same model, but add different dialogue, change their models later on
+- Add enlarged cube underneath or fix the road so there aren't any gaps
+- Polish the city and sort out the buildings
+
+*/
